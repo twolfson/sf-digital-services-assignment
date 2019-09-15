a0be5ae0e05692d0243ec12846119541c900e059
Todd Wolfson <todd@twolfson.com>
2019-09-14 18:20:24 -0700

    Finished handling errors and loading data

--- browser/js/index.js
+++ browser/js/index.js
@@ -4,17 +4,23 @@ var MOHCD_DATA_URL = 'https://data.sfgov.org/resource/9rdx-httc.json';
 // When the page is loaded
 // https://caniuse.com/#feat=domcontentloaded
 window.addEventListener('DOMContentLoaded', function handleDOMContentLoaded (evt) {
-  // Define our output handlers
-  function handleData(data) {
-    console.log('DATA!', data);
+  // Start handling our dynamic content
+  // DEV: We could break this into its own component but this webpage is *very* single purpose
+  var containerEl = document.getElementById('dynamic-content');
+  if (!containerEl) {
+    throw new Error('Unable to find container element #dynamic-content');
   }
   function handleError(err) {
-    // TODO: Replace container content with failure
+    console.error(err);
+    containerEl.textContent = 'Failed to load MOHCD data. Error: ' + err.message;
+  }
+  function handleData(data) {
+    containerEl.textContent = JSON.stringify(data, null, 2);
+    containerEl.style.cssText = 'white-space: pre; font-family: monospace';
   }
 
   // Load our dataset
   // DEV: We could use a library (e.g. jQuery, request) for this but it's simple/small enough
-  // DEV: We could break this into its own component but this webpage isn't at the scale yet
   // DEV: We are skipping the ActiveX version as we don't need to support IE6
   //   https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest_in_IE6
   // http://youmightnotneedjquery.com/#json
